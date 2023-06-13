269fa7107ba638e9003e327621aabd9bf165e300
Maximiliano Chalin <mchalin@gmail.com>
2023-06-13 18:19:43 -0300

    Pool de conexiones con Python y Postgresql
    POOL DE CONEXIONES CON PYTHON Y POSTGRESQL: Parte 1 y 2
    Obtener una conexión a partir del Pool: Parte 1 y 2
    Pruebas creando objetos del Pool de conexiones

--- PythonTercerSemestre/Leccion07/capa_datos_persona/conexion.py
+++ PythonTercerSemestre/Leccion07/capa_datos_persona/conexion.py
@@ -1,6 +1,7 @@
 import sys
 
 import psycopg2 as bd
+from psycopg2 import pool
 from logger_base import log
 
 
@@ -12,41 +13,75 @@ class Conexion:
     _HOST = '127.0.0.1'
     _conexion = None
     _cursor = None
+    _MIN_CON = 1
+    _MAX_CON = 5
+    _pool = None
 
     @classmethod
     def obtenerConexion(cls):
-        if cls._conexion is None:
+        conexion = cls.obtenerPool().getconn()
+        log.debug(f'Conexión obtenida del pool: {conexion}')
+        return conexion
+
+        # if cls._conexion is None:
+        #     try:
+        #         cls._conexion = bd.connect(
+        #             host=cls._HOST,
+        #             user=cls._USERNAME,
+        #             password=cls._PASSWORD,
+        #             port=cls._DB_PORT,
+        #             database=cls._DATABASE
+        #         )
+        #         log.debug(f'Conexión exitosa: {cls._conexion}')
+        #         return cls._conexion
+        #     except Exception as e:
+        #         log.error(f'Ocurrió una excepción al obtener la conexión: {e}')
+        #         sys.exit()
+        # else:
+        #     return cls._conexion
+
+    @classmethod
+    def obtenerCursor(cls):
+        # if cls._cursor is None:
+        #     try:
+        #         cls._cursor = cls.obtenerConexion().cursor()
+        #         log.debug(f'Se abrió el cursor exitosamente: {cls._cursor}')
+        #         return cls._cursor
+        #     except Exception as e:
+        #         log.error(f'Ocurrió una excepción al obtener el cursor: {e}')
+        #         sys.exit()
+        # else:
+        #     return cls._cursor
+        pass
+
+    @classmethod
+    def obtenerPool(cls):
+        if cls._pool is None:
             try:
-                cls._conexion = bd.connect(
+                cls._pool = pool.SimpleConnectionPool(
+                    cls._MIN_CON,
+                    cls._MAX_CON,
                     host=cls._HOST,
                     user=cls._USERNAME,
                     password=cls._PASSWORD,
                     port=cls._DB_PORT,
                     database=cls._DATABASE
                 )
-                log.debug(f'Conexión exitosa: {cls._conexion}')
-                return cls._conexion
+                log.debug(f'Creación del pool exitosa: {cls._pool}')
+                return cls._pool
             except Exception as e:
-                log.error(f'Ocurrió una excepción al obtener la conexión: {e}')
+                log.error(f'Ocurrió una excepción al obtener el pool: {e}')
                 sys.exit()
         else:
-            return cls._conexion
-
-    @classmethod
-    def obtenerCursor(cls):
-        if cls._cursor is None:
-            try:
-                cls._cursor = cls.obtenerConexion().cursor()
-                log.debug(f'Se abrió el cursor exitosamente: {cls._cursor}')
-                return cls._cursor
-            except Exception as e:
-                log.error(f'Ocurrió una excepción al obtener el cursor: {e}')
-                sys.exit()
-        else:
-            return cls._cursor
-
+            return cls._pool
 
 if __name__ == '__main__':
     conexion1 = Conexion.obtenerConexion()
-    conexion2 = Conexion.obtenerCursor()
+    conexion2 = Conexion.obtenerConexion()
+    conexion3 = Conexion.obtenerConexion()
+    conexion4 = Conexion.obtenerConexion()
+    conexion5 = Conexion.obtenerConexion()
+    conexion6 = Conexion.obtenerConexion()
+
+
 
